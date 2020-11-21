ea4c0e37ddfec24a23a52948ae2d49d3039af2a0
serdem21 <ayducity1@gmail.com>
2020-11-20 19:13:28 -0800

    added how to watch cocomelon with my son

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -1,4 +1,7 @@
 public class Cocomelon {
 
     // I love to watch cocomelon
+    // I love to watch cocomelon with my kid d
+    // I love to watch cocomelon with my kid and his friends
+    // I love to watch cocomelon with my kid and his friends, it is really fun
 }
